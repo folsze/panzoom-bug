da6c05591527d4315f4bf992fe25d45faeb15200
folsze <81622754+folsze@users.noreply.github.com>
2023-03-21 12:08:15 +0100

    bug with panning-lag is active

--- src/app/anvaka-panzoom/anvaka-panzoom.page.ts
+++ src/app/anvaka-panzoom/anvaka-panzoom.page.ts
@@ -17,7 +17,19 @@ export class AnvakaPanzoomPage {
       maxZoom: 10,
       minZoom: 1,
       bounds: true,
-      boundsPadding: 1
+      boundsPadding: 1,
+      onTouch: function(e) { // comment those three lines to make the bug disappear/appear
+        return false; // tells the library to not preventDefault.
+      }
+    });
+
+    this.addClickListeners();
+  }
+
+  public addClickListeners() {
+    const elements = Array.from(document.getElementsByClassName('clickable'));
+    elements.forEach(element => {
+      element.addEventListener('click', () => console.log('CLICK REGISTERED'));
     });
   }
 
